94ab2cb440f8e03301136dc853fc018f22a98723
ted whalen <tewhalen@gmail.com>
2025-12-08 16:28:53 -0600

    Remove unused known_joshi and non_joshi sets
    Deleted the known_joshi and non_joshi sets from joshi_data.py as they are no longer used in the code. This helps clean up the file and reduce unnecessary clutter.

--- joshirank/joshi_data.py
+++ joshirank/joshi_data.py
@@ -33,101 +33,6 @@ joshi_promotions = {
     "UpTown",
 }
 
-known_joshi = {
-    "17272",  # Willow
-    "16997",  # Jungle
-    "11386",  # Alex Lee
-    "16871",  # Charli Evans
-    "22290",  # Masha Slamovich
-    "20230",  # Banny
-    "21322",  # crane yu
-    "2114",  # michiko
-    "21687",  # pretty ota
-    "13902",  # Natsu sumire
-    "14276",  # rina yamashita
-    "26542",  # echika
-    "25256",  # maya fukuda
-    "4898",  # Kazuki
-    "27827",  # riara
-    "27601",  # chairo
-    "23863",  # MIKA
-    "18417",  # Yuina
-    "3788",  # Yuu Yamagata
-    "5962",  # Miss Mongol
-    "4913",
-    "6208",
-    "15711",
-    "3761",
-    "20459",  # the other mizuki
-    "19159",  # sae
-    "21496",  # rhythm
-    "19810",
-    "26350",
-    "18216",  # marika
-    "16375",
-    "13375",
-}
-
-non_joshi = {
-    "22354",
-    "21402",
-    "6337",
-    "22355",
-    "5474",
-    "8406",
-    "16374",
-    "1854",
-    "11317",
-    "12598",
-    "25241",
-    "3800",
-    "12878",
-    "3972",
-    "26755",
-    "24625",
-    "3265",
-    "3792",
-    "3887",
-    "4568",
-    "9849",
-    "19426",
-    "15750",
-    "18548",
-    "3760",  # gabai ji-chan
-    "20176",  # shoki kitamura
-    "26614",  # yuki toki
-    "27616",  # munetatsu
-    "26790",  # dr gore
-    "17161",  # andrew tang
-    "5266",  # chon shiryu
-    "25474",
-    "27566",
-    "6198",
-    "18752",
-    "20364",  # akki
-    "26525",
-    "15907",
-    "2266",
-    "22696",
-    "20672",
-    "20147",
-    "20163",
-    "21144",
-    "7169",
-    "5473",
-    "24590",
-    "24577",
-    "12464",
-    "27526",
-    "9162",
-    "22588",  # andreza
-    "25608",
-    "25608",
-    "8929",
-    "8796",
-    "8931",  # bison tagai
-}
-
 
 promotion_map = {
     "21342": "666",
@@ -186,6 +91,3 @@ considered_female = {
     26191,  # sazzy boatright
     9232,  # seleziya sparx
 }
-
-
-# name mapping for known wrestlers with inconsistent naming
