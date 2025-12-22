1d649d58dc79de4e88f1689c64a236a1d3fdbef8
ted whalen <tewhalen@gmail.com>
2025-12-22 13:29:19 -0600

    Update imports for Player and Record modules
    Changed imports to use joshirank.ranking.glicko2.Player and joshirank.ranking.record.Record instead of previous paths. This aligns with the updated project structure for ranking-related modules.

--- do_rank.py
+++ do_rank.py
@@ -5,9 +5,9 @@ import jinja2
 from tabulate import tabulate
 
 from joshirank.all_matches import all_matches
-from joshirank.glicko2 import Player
 from joshirank.joshidb import get_name, get_promotion_with_location
-from joshirank.record import Record
+from joshirank.ranking.glicko2 import Player
+from joshirank.ranking.record import Record
 
 URL_TEMPLATE = "https://www.cagematch.net/?id=2&nr={w_id}&view=&page=4&gimmick=&year={year}&promotion=&region=&location=&arena=&showtype=&constellationType=Singles&worker="
 
